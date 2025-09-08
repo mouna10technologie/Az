19289103651331a81ebaefb0cdd5db6fe46e9cdb
mouna <mounakhallou@outlook.com>
2025-09-08 16:34:08 +0100

    az changement

--- src/IA.jsx
+++ src/IA.jsx
@@ -1,11 +1,115 @@
 import "./IA.css";
+function CarteAi({ titre1, description1 }) {
+  return (
+    <div className="div_ai">
+      <h2>{titre1}</h2>
+      <p>{description1}</p>
+    </div>
+  );
+}
+
+function CarteIa({ titre }) {
+  return (
+    <div className="div_ia">
+      <h2>{titre}</h2>
+    </div>
+  );
+}
 function IA() {
+  let box4 = [
+    {
+      titre: "Analyse rapide et ciblée des candidatures",
+    },
+
+    {
+      titre: "Identification des meilleurs profils",
+    },
+
+    {
+      titre: "Automatisation des étapes répétitives",
+    },
+  ];
+
+  let box5 = [
+    {
+      titre1: "IA générative (Generative AI)",
+      description1:
+        "L’IA générative assiste les développeurs en automatisant la création, la complétion et la traduction de code.",
+    },
+
+    {
+      titre1: "IA pour l’analyse statique et le débogage",
+      description1:
+        "L’IA pour l’analyse statique et le débogage détecte automatiquement les erreurs, vulnérabilités et inefficacités dans le code, tout en proposant des améliorations.",
+    },
+
+    {
+      titre1: "IA pour l’optimisation et la performance",
+      description1:
+        "L’IA pour l’optimisation améliore les performances du code et de l’infrastructure, tandis que l’IA conversationnelle assiste les développeurs en répondant à leurs questions et en expliquant le code en temps réel.",
+    },
+    {
+      titre1: "IA conversationnelle (chatbots et assistants intelligents)",
+      description1:
+        "L’IA conversationnelle aide les développeurs en répondant à leurs questions, en expliquant du code et en facilitant l’accès à la documentation.",
+    },
+    {
+      titre1: "IA pour les tests automatisés",
+      description1:
+        "L’IA pour les tests automatisés génère et exécute des tests logiciels afin de détecter bugs, régressions et erreurs en production.",
+    },
+    {
+      titre1: "IA pour la gestion de projet et DevOps",
+      description1:
+        "L’IA pour la gestion de projet et le DevOps automatise la planification, le suivi des tâches et l’exécution intelligente des workflows CI/CD.",
+    },
+    {
+      titre1: "IA pour l’apprentissage des développeurs",
+      description1:
+        "Plateformes comme LeetCode avec IA, Exercism AI mentor, ou Kaggle Notebooks avec assistant AI peuvent accélérer l’apprentissage des concepts de programmation.",
+    },
+  ];
+
   return (
     <>
       <div className="intelligenceArtificielle">
-        <img className="image_ai" src="./public/ia.jpeg " />
+        <img
+          className="image_ai"
+          alt="image sur l'intelligence artificielle"
+          src="./public/ia.jpeg"
+        />
         <h1 className="h1_ia">L’intelligence artificielle</h1>
-        <h3 className="h3_ai">L’intelligence artificielle facilite le recrutement en analysant rapidement les CV, en identifiant les meilleurs profils et en automatisant certaines étapes, ce qui rend le processus plus rapide et efficace. Cependant, elle pose aussi des enjeux éthiques, notamment sur la transparence et la protection des données des candidats.</h3>
+        <h3 className="h3_ai">
+          L’intelligence artificielle facilite le recrutement en analysant
+          rapidement les CV, en identifiant les meilleurs profils et en
+          automatisant certaines étapes, ce qui rend le processus plus rapide et
+          efficace. Cependant, elle pose aussi des enjeux éthiques, notamment
+          sur la transparence et la protection des données des candidats.
+        </h3>
+      </div>
+      <div className="div_ai1">
+        <p className="p_ia">
+          le rôle de l’intelligence artificielle dans le recrutement
+        </p>
+      </div>
+      <div className="Ia">
+        {box4.map((valeur, index5) => (
+          <CarteIa key={index5} titre={valeur.titre} />
+        ))}
+      </div>
+      <div>
+        <p className="p_ia1">
+          Les types d’IA qui peuvent faciliter le travail des développeurs
+        </p>
+      </div>
+      <div className="Ia1">
+        {box5.map((valeur1, index6) => (
+          <CarteAi
+            key={index6}
+            titre1={valeur1.titre1}
+            description1={valeur1.description1}
+          />
+        ))}
       </div>
     </>
   );
--- src/Recruteurs.jsx
+++ src/Recruteurs.jsx
@@ -1,38 +1,98 @@
 import "./Recruteurs.css";
+function CarteRecruteurs1({ image, lien }) {
+  return (
+    <div className="div_reruteurs1">
+      <a href={lien} target="_blank" rel="noopener noreferrer">
+        <img
+          src={image}
+          alt="image des sites freelance"
+          width="150px"
+          height="150px"
+        />
+      </a>
+    </div>
+  );
+}
+
 function CarteRecruteurs({ titre, description }) {
   return (
     <div className="div_recruteurs">
       <h2>{titre}</h2>
       <h4>{description}</h4>
     </div>
-  )
+  );
 }
 function Recruteurs() {
-  let box2 = [{
-    titre: "Plateformes de sourcing et publication d’offres",
-    description: "es plateformes de sourcing et de publication d’offres incluent LinkedIn Recruiter pour contacter directement des développeurs, des sites d’annonces comme Indeed, Monster et Glassdoor, GitHub pour identifier et évaluer les contributions des développeurs, Stack Overflow Jobs pour les profils tech spécialisés, et AngelList pour recruter dans les startups.",
-  },
+  let box2 = [
+    {
+      titre: "Plateformes de sourcing et publication d’offres",
+      description:
+        "Les plateformes de sourcing et de publication d’offres incluent LinkedIn Recruiter pour contacter directement des développeurs, des sites d’annonces comme Indeed, Monster et Glassdoor, GitHub pour identifier et évaluer les contributions des développeurs, Stack Overflow Jobs pour les profils tech spécialisés, et AngelList pour recruter dans les startups.",
+    },
 
-  {
-    titre: "Outils de gestion du recrutement (ATS - Applicant Tracking Systems)",
-    description: "Les ATS (Applicant Tracking Systems) comme Greenhouse, Lever, SmartRecruiters, Workable et Breezy HR permettent de centraliser les candidatures, gérer le pipeline de recrutement et intégrer des tests techniques.",
-  },
+    {
+      titre:
+        "Outils de gestion du recrutement (ATS - Applicant Tracking Systems)",
+      description:
+        "Les ATS (Applicant Tracking Systems) comme Greenhouse, Lever, SmartRecruiters, Workable et Breezy HR permettent de centraliser les candidatures, gérer le pipeline de recrutement et intégrer des tests techniques.",
+    },
 
-  {
-    titre: "Tests techniques et évaluation des compétences",
-    description: "Les outils de tests techniques comme Codility, HackerRank, LeetCode, DevSkiller et Kaggle permettent d’évaluer les compétences des développeurs et data scientists via des exercices de codage et projets réels.",
-  },
+    {
+      titre: "Tests techniques et évaluation des compétences",
+      description:
+        "Les outils de tests techniques comme Codility, HackerRank, LeetCode, DevSkiller et Kaggle permettent d’évaluer les compétences des développeurs et data scientists via des exercices de codage et projets réels.",
+    },
 
-  {
-    titre: " Outils de vidéo conférence",
-    description: "Les outils de vidéoconférence comme Zoom, Microsoft Teams, Google Meet et Whereby sont utilisés pour réaliser des entretiens à distance.",
-  },
-  {
-    titre: "Outils de communication et collaboration interne",
-    description: "Les outils comme Slack et Microsoft Teams facilitent la communication et la collaboration entre recruteurs et équipes techniques.",
-  },
-  
-  ]
+    {
+      titre: " Outils de vidéo conférence",
+      description:
+        "Les outils de vidéoconférence comme Zoom, Microsoft Teams, Google Meet et Whereby sont utilisés pour réaliser des entretiens à distance.",
+    },
+    {
+      titre: "Outils de communication et collaboration interne",
+      description:
+        "Les outils comme Slack et Microsoft Teams facilitent la communication et la collaboration entre recruteurs et équipes techniques.",
+    },
+  ];
+
+  let box6 = [
+    { image: "./public/Codeur-logo.png", lien: "https://www.codeur.com/" },
+
+    { image: "./public/Freelancer.webp", lien: "https://www.freelancer.com/" },
+
+    {
+      image: "./public/Fiverr.png",
+      lien: "https://www.fiverr.com/?msockid=0bd7572cab456563326442e2aa3c6404",
+    },
+
+    { image: "./public/Guru.jpg", lien: "https://www.guru.com/" },
+
+    {
+      image: "./public/Euros5.webp",
+      lien: "https://comeup.com/en/5euros-devient-comeup",
+    },
+
+    {
+      image: "./public/Le-studio.webp",
+      lien: "https://www.bing.com/search?q=Le+Studio+Tech&FORM=HDRSC1",
+    },
+
+    { image: "./public/LeHibou.png", lien: "https://www.lehibou.com/" },
+    { image: "Linkedin.webp", lien: "https://fr.linkedin.com/" },
+
+    { image: "./public/Malt.webp", lien: "https://www.malt.fr/" },
+
+    { image: "./public/Toptal.png", lien: "https://www.toptal.com/" },
+    { image: "./public/Upwork.webp", lien: "https://www.upwork.com/" },
+    {
+      image: "./public/PeoplePerHour-logo1.png",
+      lien: "https://www.peopleperhour.com/",
+    },
+    {
+      image: "./public/Creme.jpg",
+      lien: "https://www.cremedelacreme.io/?utm_source=chatgpt.com",
+    },
+  ];
 
   return (
     <>
@@ -42,24 +102,40 @@ function Recruteurs() {
         <h3 className="h3_recruteurs">
           Le recruteur est un professionnel qui identifie et sélectionne les
           meilleurs profils pour répondre aux besoins d’une entreprise,
-          notamment dans le secteur tech.**
+          notamment dans le secteur tech.
         </h3>
       </div>
- <div className="div_recruter"><p className="p_recruteur">Les outils couramment utilisés par les recruteurs pour le recrutement des développeurs</p></div>
-     <div className="Recruteurs">
+      <div className="div_recruter">
+        <p className="p_recruteur">
+          Les outils couramment utilisés par les recruteurs pour le recrutement
+          des développeurs
+        </p>
+      </div>
+      <div className="Recruteurs">
         {box2.map((valeur, index3) => (
           <CarteRecruteurs
             key={index3}
             titre={valeur.titre}
             description={valeur.description}
-
           />
         ))}
       </div>
-
-
-
-
+      <div>
+        <p className="p_recruteurs1">
+          Découvrez une sélection des sites de recrutement pour développeurs,
+          ainsi que des plateformes freelance pour trouver des missions ou
+          recruter des talents tech.
+        </p>
+      </div>
+      <div className="Recruteurs1">
+        {box6.map((valeur1, index7) => (
+          <CarteRecruteurs1
+            key={index7}
+            image={valeur1.image}
+            lien={valeur1.lien}
+          />
+        ))}
+      </div>
     </>
   );
 }
